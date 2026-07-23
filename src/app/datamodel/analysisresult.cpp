e0c45a6bd5c98a2c40b8777274f9a98a373ff89e
DomaszDomingo <nowakowski.dominik93@gmail.com>
2026-07-23 20:58:47 +0200

    StworzenieBrancha

---
+++ src/app/datamodel/analysisresult.cpp
@@ -0,0 +1,3 @@
+#include "analysisresult.h"
+
+AnalysisResult::AnalysisResult() {}
---
+++ src/app/datamodel/analysisresult.h
@@ -0,0 +1,10 @@
+#ifndef ANALYSISRESULT_H
+#define ANALYSISRESULT_H
+
+class AnalysisResult
+{
+public:
+    AnalysisResult();
+};
+
+#endif // ANALYSISRESULT_H
